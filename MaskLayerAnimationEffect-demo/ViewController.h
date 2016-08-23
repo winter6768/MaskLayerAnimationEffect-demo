0106f71af46c18b7f77b0700768574b0e1ff79b9
yangyandong <yangyandong@yesshou.com>
2016-08-23 16:13:19 +0800

    Initial Commit

---
+++ MaskLayerAnimationEffect-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaskLayerAnimationEffect-demo
+//
+//  Created by 也瘦 on 16/8/23.
+//  Copyright © 2016年 也瘦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MaskLayerAnimationEffect-demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MaskLayerAnimationEffect-demo
+//
+//  Created by 也瘦 on 16/8/23.
+//  Copyright © 2016年 也瘦. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
